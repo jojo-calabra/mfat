4b43a227c596876b41802f27f9180e9d846bc113
Jojo Calabra <130367645+jojo-calabra@users.noreply.github.com>
2023-05-14 01:38:54 +0200

    empty server main file, might be renamed and moved

---
+++ src/api_server_cpp/server.cpp
@@ -0,0 +1,19 @@
+// Protobuf & gRPC (C++ gRPC Server for cluster communication)
+// crow (C++ REST/HTTP Server for API)
+// openssl (for https)
+// boost (for all kinds of uses, theres over 160 libraries included in boost)
+
+// standard libraries
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+#include <regex>
+
+// non-standard libraries
+// #include <crow.h>
+// #include <grpcpp/grpcpp.h>
+// #include <cryptopp/pwdbased.h>
+// #include <cryptopp/sha.h>
+// #include <cryptopp/hex.h>
