1afa34f02d55d5eb8882adcb14f23ecb0fe1a5ca
Jojo Calabra <130367645+jojo-calabra@users.noreply.github.com>
2023-05-15 05:46:12 +0200

    updated ports loading to load from environment instead of a physical file

--- src/app_vue3/vite.config.ts
+++ src/app_vue3/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// load environment variables from .env file
-import dotenv from 'dotenv'
-import path from 'path'
-dotenv.config({ path: path.resolve(__dirname, '../../.env') }) // load .env file from root directory
+// load environment variables from environment variables
+import { loadEnv } from 'vite'
+const env = loadEnv('', process.cwd())
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {  // Exposes the server to the docker internal network
     host: true,
-    port: Number(process.env.VUE3_APP_PORT_DOCKER) || 56123, // load port from .env file or use default value
+    // load port from environment variables otherwies use default port 56123
+    port: Number(env.VITE_PORT || 5173),
   },
 })
